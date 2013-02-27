e552e35bb5bc3f7a27dc2a938dc98f7fa0c1ade2
Kevin Lynch <klynch@gmail.com>
2013-02-27 00:39:32 -0500

    add d3 map sample

--- project/controllers/index.py
+++ project/controllers/index.py
@@ -6,3 +6,8 @@ from bottle import template, request
 @app.route('/', method='GET')
 def index():
     return template('home/index', message='')
+
+
+@app.route('/map', method=['GET'])
+def cartogram():
+    return template('home/map.html')
